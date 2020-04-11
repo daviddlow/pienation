4e53253a02f06c47eb8812a35730e1942f98481e
David Low <david.low@list.co.uk>
2020-04-11 17:28:33 +0100

    redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,9 @@ module.exports = {
   },
   plugins: [
     {
+	  resolve: `gatsby-plugin-netlify`,
+	}
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
