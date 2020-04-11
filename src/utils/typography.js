f17dd96fc2383f9683403b63aff681797481fba3
David Low <david.low@list.co.uk>
2020-04-11 17:06:44 +0100

    Typography change

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from "typography"
-import wikipediaTheme from "typography-theme-Wikipedia"
+import Wordpress2016 from "typography-theme-wordpress-2016"
 
-wikipediaTheme.overrideThemeStyles = () => {
+Wordpress2016.overrideThemeStyles = () => {
   return {
     "a.gatsby-resp-image-link": {
       boxShadow: `none`,
@@ -9,9 +9,9 @@ wikipediaTheme.overrideThemeStyles = () => {
   }
 }
 
-delete wikipediaTheme.googleFonts
+delete Wordpress2016.googleFonts
 
-const typography = new Typography(wikipediaTheme)
+const typography = new Typography(Wordpress2016)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
